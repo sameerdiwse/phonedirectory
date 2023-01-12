28008142e7a130ca297358c675c53d13283d5db6
sameerdiwse <sameerdiwse9090@gmail.com>
2023-01-12 17:13:18 +0530

    pushing changes

--- src/main/java/com/smartcontactmanager/entity/Contact.java
+++ src/main/java/com/smartcontactmanager/entity/Contact.java
@@ -1,5 +1,31 @@
 package com.smartcontactmanager.entity;
 
-public class Contact {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+
+@Entity
+@Table(name="")
+@Data
+public class Contact 
+{ 
+   @Id	
+   private int cid;
+   @Column(name="")
+   private int phone;
+   @Column(name="name")
+   private String description;
+   @Column(name="")
+   private String name;
+   @Column(name="")
+   private String nickName;
+   @Column(name="")
+   private String profession;
+   @Column(name="")
+   private String email;
+   @Column(name="")
+   private String imageUrl;
 }
--- src/main/java/com/smartcontactmanager/entity/User.java
+++ src/main/java/com/smartcontactmanager/entity/User.java
@@ -1,18 +1,31 @@
 package com.smartcontactmanager.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Data;
 
 @Entity
 @Table(name="UserTable")
+@Data
 public class User 
 {
+	@Id
    private int id;
+	@Column(name="")
    private String email;
+	@Column(name = "")
    private String password;
+	@Column(name="")
    private String imageUrl;
+	@Column(name="")
    private String about;
+	@Column(name="")
    private String name;
+	@Column(name="")
    private String role;
+	@Column(name="")
    private boolean enabled;
 }
