c9260e757ce11c7e7498fc031ca43d5d2623e17f
sameerdiwse <sameerdiwse9090@gmail.com>
2023-01-30 22:19:25 +0530

    pushing now

--- src/main/java/com/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smartcontactmanager/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -58,7 +59,7 @@ public class UserController
     	 return "Registration";
     }
     
-    @PatchMapping("/updateuser")
+    @PutMapping("/updateuser")
     public void updateUser(User user)
     {
     	userService.updateUser(user);
